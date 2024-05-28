d98dde8bc3993f35f5bff242addf109edae0ed2e
pujitha-29 <Nagasandeep.chinni@gmail.com>
2024-05-28 22:05:54 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1" # Replace with your desired AWS region
+  region = "eu-west-1" # Replace with your desired AWS region
    access_key = "AKIAUHQSU2XRKTTZFQ7J"
   secret_key = "dOu3PPh0QGS792DOal7z4B0OwqMMHutQXh6xHCe"
 }
